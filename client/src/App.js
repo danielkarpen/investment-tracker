a708226e520f842c3c0fc000c7305fc3762e06b7
Daniel <danielkarpen97@gmail.com>
2021-03-23 16:37:20 -0500

    authcontext global

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,51 @@
 import { Box, ChakraProvider, Grid, theme } from '@chakra-ui/react';
+import api from 'api';
+import { AuthContext } from 'context';
 import Layout from 'layout';
-import { AboutPage, HomePage, LoginPage } from 'pages';
+import { AboutPage, DashboardPage, HomePage, LoginPage } from 'pages';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
-
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const user = await api.auth.show();
+      if (user) {
+        setLoggedInUser(user);
+      }
+    })();
+  });
+
+  function toggleUser(user) {
+    setLoggedInUser(() => user || null);
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <Layout>
-            <Router>
-              <Switch>
-                <Route exact path="/">
-                  <HomePage />
-                </Route>
-                <Route exact path="/about">
-                  <AboutPage />
-                </Route>
-                <Route exact path="/login">
-                  <LoginPage />
-                </Route>
-              </Switch>
-            </Router>
-          </Layout>
+          <AuthContext.Provider value={{ loggedInUser, toggleUser }}>
+            <Layout>
+              <Router>
+                <Switch>
+                  <Route exact path="/">
+                    <HomePage />
+                  </Route>
+                  <Route exact path="/dashboard">
+                    <DashboardPage />
+                  </Route>
+                  <Route exact path="/about">
+                    <AboutPage />
+                  </Route>
+                  <Route exact path="/login">
+                    <LoginPage />
+                  </Route>
+                </Switch>
+              </Router>
+            </Layout>
+          </AuthContext.Provider>
         </Grid>
       </Box>
     </ChakraProvider>
