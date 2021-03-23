f4d880562cff668192d58333e58a597221ed1f97
Daniel <danielkarpen97@gmail.com>
2021-03-23 16:46:55 -0500

    log out button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,34 @@
-import { Heading } from '@chakra-ui/react';
+import { Button, Heading } from '@chakra-ui/react';
+import api from 'api';
 import { ColorModeSwitcher } from 'ColorModeSwitcher';
+import { AuthContext } from 'context';
+import { useContext } from 'react';
 
-const Header = () => (
-  <header className="flex justify-between">
-    <Heading fontSize="xl">My Capstone!</Heading>
-    <ColorModeSwitcher />
-  </header>
-);
+const Header = () => {
+  const { loggedInUser, toggleUser } = useContext(AuthContext);
 
+  function handleClick() {
+    api.auth
+      .delete()
+      .catch(error => {
+        console.error(error.msg);
+      })
+
+      // Fail or not - reset the user
+      .finally(() => {
+        toggleUser(null);
+      });
+  }
+  return (
+    <header className="flex justify-between">
+      <Heading fontSize="xl">Investment Tracker</Heading>
+      {loggedInUser ? (
+        <Button colorScheme="orange" size="sm" onClick={handleClick}>
+          Logout
+        </Button>
+      ) : null}
+      <ColorModeSwitcher />
+    </header>
+  );
+};
 export default Header;
