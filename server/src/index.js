049240cc8bd74e800d9a325c1054251806cafcf9
Daniel <danielkarpen97@gmail.com>
2021-03-23 18:41:23 -0500

    refactor example to investments

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
+import cors from "cors";
 import express from "express";
 import config from "./config";
-import { example } from "./routes";
+import { investments } from "./routes";
 
 const app = express();
 
@@ -8,11 +9,12 @@ app.get("/", (_, res) => {
   res.send("<h1>Hello Express!</h1>");
 });
 
+app.use(cors({ origin: "http://localhost:3000" }));
 // All routes will be receiving JSON in `req.body`
 app.use(express.json());
 
 // Route any requests made with path '/example'...
-app.use("/example", example);
+app.use("/investments", investments);
 
 app.listen(process.env.PORT, () => {
   console.info(`Express server 🏃🏾‍♂️ on port: ${config.port}`);
--- server/src/routes/investments.js
+++ server/src/routes/investments.js
@@ -4,7 +4,11 @@ const router = new Router();
 
 // '/example/'
 router.get("/", (_, res) => {
-  res.send("<h1>Hello Example GET Route!</h1>");
+  res.send("<h1>Investments</h1>");
+});
+
+router.post("/", (req, res) => {
+  res.json(req.body);
 });
 
 export default router;
