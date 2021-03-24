d763323a89fa66eedd70d4526f544d04ceff295b
Daniel <danielkarpen97@gmail.com>
2021-03-24 10:44:01 -0500

    test db connection

--- server/src/routes/investments.js
+++ server/src/routes/investments.js
@@ -1,3 +1,4 @@
+import db from "db";
 import { Router } from "express";
 
 const router = new Router();
@@ -7,8 +8,13 @@ router.get("/", (_, res) => {
   res.send("<h1>Investments</h1>");
 });
 
-router.post("/", (req, res) => {
-  res.json(req.body);
+router.post("/", async (req, res) => {
+  try {
+    const results = await db.addThrowawayData(req.body);
+    res.json(results);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 export default router;
