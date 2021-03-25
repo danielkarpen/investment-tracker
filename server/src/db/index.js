6f3edafab3cd5398ea24c34bed7a3f825e19f002
Daniel <danielkarpen97@gmail.com>
2021-03-25 17:38:08 -0500

    add service routes for get and add investments

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,12 +1,33 @@
 import client from "./client";
-
+// database service methods
 export default {
-  async addThrowawayData(payload) {
+  async addInvestment(payload) {
     try {
       const results = await client
         .db("investments")
         .collection("data")
         .insertOne(payload);
+      return results;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+  async getInvestments() {
+    try {
+      const cursor = await client.db("investments").collection("data").find();
+      const results = await cursor.toArray();
+      return results;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+  async addPartnerToInvestment(investment, partner) {
+    try {
+      const results = await client
+        .db("investments")
+        .collection("data")
+        .updateOne({ investment }, { $push: { partners: partner } });
+      // ^ inside of investment object update partners array with new partner object
       return results;
     } catch (err) {
       throw new Error(err);
