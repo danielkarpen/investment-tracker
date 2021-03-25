77c724fda35ed4ddca2486614d89089d6fc9dee6
Daniel <danielkarpen97@gmail.com>
2021-03-25 18:30:06 -0500

    check for admin email

--- server/src/routes/investments.js
+++ server/src/routes/investments.js
@@ -1,16 +1,20 @@
+import config from "config";
 import db from "db";
 import { Router } from "express";
 
 const router = new Router();
 
-// '/example/'
+// route for investments
 router.get("/", (_, res) => {
   res.send("<h1>Investments</h1>");
 });
 
 router.post("/", async (req, res) => {
   try {
-    const results = await db.addThrowawayData(req.body);
+    if (req.body.email !== config.admin) {
+      throw new Error("Unauthorized");
+    }
+    const results = await db.getAllInvestments(req.body.email);
     res.json(results);
   } catch (err) {
     res.status(500).json({ error: err.message });
