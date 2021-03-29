214921c0e3bcecacb52e22c2a7c8730757f9f7ae
Daniel <danielkarpen97@gmail.com>
2021-03-29 16:08:22 -0500

    server routes

--- server/src/routes/investments.js
+++ server/src/routes/investments.js
@@ -10,15 +10,48 @@ router.get("/", (_, res) => {
 });
 
 router.post("/", async (req, res) => {
+  console.log("got a request", req.body.email);
   try {
     if (req.body.email !== config.admin) {
       throw new Error("Unauthorized");
     }
-    const results = await db.getAllInvestments(req.body.email);
+    const results = await db.getAllInvestments();
     res.json(results);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
+
+router.post("/user", async (req, res) => {
+  try {
+    const results = await db.getUserInvestments(req.body.email);
+    res.json(results);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/investment", async ({ body: { investment } } = {}, res) => {
+  try {
+    if (!investment) {
+      res.status(400).json({ error: "Invalid investment name!" });
+      return;
+    }
+    res.status(201).json(await db.addInvestment(investment));
+  } catch (error) {
+    if (error.name === "MongoError") {
+      res.status(500).json({ error: error.message });
+    }
+
+    // Probably invalid data in the request
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// router.delete("/user", async (req, res) => {
+//   try {
+//     const r;
+//   }
+// });
 
 export default router;
