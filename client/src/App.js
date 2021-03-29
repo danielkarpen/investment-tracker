94a006480175185f97186fba1758a72207396e91
Daniel <danielkarpen97@gmail.com>
2021-03-29 16:06:07 -0500

    queryclient

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,13 @@ import { AuthContext } from 'context';
 import Layout from 'layout';
 import { AboutPage, DashboardPage, HomePage, LoginPage, ViewPage } from 'pages';
 import { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
+
+const queryClient = new QueryClient();
+
 function App() {
   const [loggedInUser, setLoggedInUser] = useState(null);
 
@@ -27,27 +32,30 @@ function App() {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <AuthContext.Provider value={{ loggedInUser, toggleUser }}>
-            <Layout>
-              <Router>
-                <Switch>
-                  <Route exact path="/">
-                    <HomePage />
-                  </Route>
-                  <Route exact path="/dashboard">
-                    <DashboardPage />
-                  </Route>
-                  <Route exact path="/about">
-                    <AboutPage />
-                  </Route>
-                  <Route exact path="/login">
-                    <LoginPage />
-                  </Route>
-                  <Route exact path="/view">
-                    <ViewPage />
-                  </Route>
-                </Switch>
-              </Router>
-            </Layout>
+            <QueryClientProvider client={queryClient}>
+              <Layout>
+                <Router>
+                  <Switch>
+                    <Route exact path="/">
+                      <HomePage />
+                    </Route>
+                    <Route path="/dashboard">
+                      <DashboardPage />
+                    </Route>
+                    <Route exact path="/about">
+                      <AboutPage />
+                    </Route>
+                    <Route exact path="/login">
+                      <LoginPage />
+                    </Route>
+                    <Route exact path="/view">
+                      <ViewPage />
+                    </Route>
+                  </Switch>
+                </Router>
+              </Layout>
+              <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
           </AuthContext.Provider>
         </Grid>
       </Box>
